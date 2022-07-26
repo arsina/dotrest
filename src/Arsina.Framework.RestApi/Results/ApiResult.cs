35d09c088183a17aa7e99a33640569ddc6055829
Amir Naderi <amir.naderi@arsina.net>
2022-07-26 09:15:20 +0430

    Support all data types in ApiResult class

--- src/Arsina.Framework.RestApi/Results/ApiResult.cs
+++ src/Arsina.Framework.RestApi/Results/ApiResult.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace Arsina.Framework.RestApi.Results
 {
-	public class ApiResult<T> : ResultBase where T : class
+	public class ApiResult<T> : ResultBase
 	{
 		public ApiResult() : base(false, "")
 		{
 
 		}
-		public ApiResult(bool isSuccessful, T data = null, string message = null)
+		public ApiResult(bool isSuccessful, T data = default, string message = null)
 			: base(isSuccessful, message)
 		{
 			this.Data = data;
